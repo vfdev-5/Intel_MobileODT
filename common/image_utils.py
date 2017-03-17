d6bdee03c45cb973de95d0e3e4e12c8d99afd55f
vfdev-5 <vfdev.5@gmail.com>
2017-03-17 13:56:19 +0000

    * Use PIL to read image

--- common/image_utils.py
+++ common/image_utils.py
@@ -1,5 +1,6 @@
 
 import cv2
+from PIL import Image
 
 # Project
 from data_utils import get_filename
@@ -9,8 +10,27 @@ def get_image_data(image_id, image_type):
     """
     Method to get image data as np.array specifying image id and type
     """
+    return _get_image_data_pil(image_id, image_type)
+
+
+def _get_image_data_opencv(image_id, image_type):
+    """
+    Method to get image data as np.array specifying image id and type
+    """
     fname = get_filename(image_id, image_type)
     img = cv2.imread(fname)
     assert img is not None, "Failed to read image : %s, %s" % (image_id, image_type)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
+
+
+def _get_image_data_pil(image_id, image_type):
+    """
+    Method to get image data as np.array specifying image id and type
+    """
+    fname = get_filename(image_id, image_type)
+    try:
+        img = Image.open(fname)
+    except Exception as e:
+        assert False, "Failed to read image : %s, %s. Error message: %s" % (image_id, image_type, e)
     return img
