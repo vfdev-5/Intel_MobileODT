c0356470c96970b5b8b766322f2debb57b3d6ebb
vfdev-5 <vfdev.5@gmail.com>
2017-03-17 11:16:04 +0000

    * Remove copied code

--- scripts/test_data_iterator.py
+++ scripts/test_data_iterator.py
@@ -81,12 +81,7 @@ if __name__ == "__main__":
 
     batch_size = 16
     if 'c001' in platform.node():
-        batch_size = 128
-        print("-- On the cluster --")
-
-    batch_size = 16
-    if 'c001' in platform.node():
-        batch_size = 128
+        batch_size = 512
         print("-- On the cluster --")
 
     print("\n {} - Get train/val lists ...".format(datetime.datetime.now()))
