ec3109a851b12be32db1ba342f8d52d75634dd1b
vfdev-5 <vfdev.5@gmail.com>
2017-03-21 10:17:11 -0700

    * [WIP] Update code

--- scripts/resnet_simple.py
+++ scripts/resnet_simple.py
@@ -1,7 +1,3 @@
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load in 
-
 import platform
 
 if 'c001' in platform.node():
