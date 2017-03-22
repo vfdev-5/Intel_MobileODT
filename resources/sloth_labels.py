685b6652ebf7a370937660f51f880affdbef8a59
vfdev-5 <vfdev.5@gmail.com>
2017-03-22 03:11:41 +0100

    * Add annotations

---
+++ resources/sloth_labels.py
@@ -0,0 +1,19 @@
+LABELS = (
+    {"attributes": 
+         {"type": "rect",
+          "class": "os"},
+     "item": "sloth.items.RectItem",
+     "inserter": "sloth.items.RectItemInserter",
+     "hotkey": 'q',
+     "text": "Os"   
+    },
+    {"attributes": 
+         {"type": "rect",
+          "class": "cervix"},
+     "item": "sloth.items.RectItem",
+     "inserter": "sloth.items.RectItemInserter",
+     "hotkey": 'w',
+     "text": "Cervix"
+    },
+
+)
